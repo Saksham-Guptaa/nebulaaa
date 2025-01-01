24e2762325ed3bf932dd4844ddaa9ba79e526e19
saksham-guptaa <creator.sakshamgupta@gmail.com>
2025-01-01 17:09:45 +0530

    build fix

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import { useFirebase } from "../../context/FirebaseContext";
 import UserList from "../../components/UserList/UserList";
 import PrivateChat from "../../components/PrivateChat/PrivateChat";
-import SignIn from "../auth/signin/page";
 import { auth } from "@/utils/firebase";
 
 const Home: React.FC = () => {
@@ -21,49 +20,43 @@ const Home: React.FC = () => {
       console.error("Error during logout:", error);
     });
   };
-
   return (
     <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      {user ? (
-        <>
-          <h1 style={{ color: "#333" }}>
-            Welcome, {userDetails?.fullName || user.email}
-          </h1>
-          <p>
-            <strong>Role:</strong> {userDetails?.role || "Not specified"}
-          </p>
-          <p>
-            <strong>Phone Number:</strong>{" "}
-            {userDetails?.phoneNumber || "Not available"}
-          </p>
+      <>
+        <h1 style={{ color: "#333" }}>
+          Welcome, {userDetails?.fullName || user?.email}
+        </h1>
+        <p>
+          <strong>Role:</strong> {userDetails?.role || "Not specified"}
+        </p>
+        <p>
+          <strong>Phone Number:</strong>{" "}
+          {userDetails?.phoneNumber || "Not available"}
+        </p>
+        {selectedUser ? (
+          <PrivateChat selectedUser={selectedUser} />
+        ) : (
+          <UserList
+            onSelectUser={(user) => setSelectedUser(user)}
+            currentUserUid={user?.uid || ""}
+          />
+        )}
 
-          {selectedUser ? (
-            <PrivateChat selectedUser={selectedUser} />
-          ) : (
-            <UserList
-              onSelectUser={(user) => setSelectedUser(user)}
-              currentUserUid={user.uid}
-            />
-          )}
-
-          <button
-            onClick={handleLogout}
-            style={{
-              marginTop: "20px",
-              padding: "10px 20px",
-              backgroundColor: "#f44336",
-              color: "#fff",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-            }}
-          >
-            Logout
-          </button>
-        </>
-      ) : (
-        <SignIn />
-      )}
+        <button
+          onClick={handleLogout}
+          style={{
+            marginTop: "20px",
+            padding: "10px 20px",
+            backgroundColor: "#f44336",
+            color: "#fff",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Logout
+        </button>
+      </>
     </div>
   );
 };
