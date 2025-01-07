2d7e8e952224ebdc1e0d4146c734d44df19cb8a2
Simran <itssimran903@gmail.com>
2025-01-07 12:50:42 +0530

    UI updated of file manager

--- src/components/FileManager/index.tsx
+++ src/components/FileManager/index.tsx
@@ -21,18 +21,19 @@ const FileManager: React.FC = () => {
       <Breadcrumb pageName="Deck" />
       <div className="p-4">
         <div className="grid grid-cols-1 dark:text-white/80 text-black">
-              <div className="shadow-xl bg-bodydark1 dark:bg-boxdark p-16 space-y-6 leading-loose">
-                <h3 className="font-semibold text-2xl mb-2">{userDetails?.fullName || "No Name"}</h3>
-                <p className="tracking-widest">
-                  <strong>Email:</strong> {userDetails?.email || "No Email"}
-                </p>
-                <p className="tracking-widest">
-                  <strong>Role:</strong> {userDetails?.role || "No Role"}
-                </p>
-                <p className="tracking-widest">
-                  <strong>Contact Number:</strong> {userDetails?.phoneNumber || "No Phone Number"}
-                </p>
-              </div>
+        {userDetails && (
+            <div className="card flex justify-between bg-white dark:bg-boxdark shadow-md rounded-lg p-4">
+              <p className="tracking-widest">
+                <strong>Name:</strong> {userDetails.fullName}
+              </p>
+              <p className="tracking-widest">
+                <strong>Email:</strong> {userDetails.email}
+              </p>
+              <p className="tracking-widest">
+                <strong>Contact Number:</strong> {userDetails.phoneNumber}
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </>
