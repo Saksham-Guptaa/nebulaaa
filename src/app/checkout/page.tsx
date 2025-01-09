9bc78c843539c8883b1e22270b549f3c7fdc36af
saksham-guptaa <creator.sakshamgupta@gmail.com>
2025-01-09 15:06:24 +0530

    build error resolving

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,21 +1,12 @@
-"use client"
-import { loadStripe, Stripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
+"use client";
 import CheckoutForm from "@/components/CheckoutForm";
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string);
 
 const Checkout: React.FC = () => {
   return (
     <div>
       <h1>Checkout</h1>
-      {stripePromise ? (
-        <Elements stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      ) : (
-        <p>Loading payment gateway...</p>
-      )}
+      <CheckoutForm />
+      <p>Loading payment gateway...</p>
     </div>
   );
 };
