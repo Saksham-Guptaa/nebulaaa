fa39e55ac6c18eba119a4052c35c8632b06b2526
saksham-guptaa <creator.sakshamgupta@gmail.com>
2025-01-15 07:45:19 +0530

    profile changes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -170,13 +170,6 @@ const Profile = () => {
                       <h4 className="font-semibold text-black dark:text-white">
                         About Me
                       </h4>
-                      <p className="mt-4.5">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Pellentesque posuere fermentum urna, eu condimentum
-                        mauris tempus ut. Donec fermentum blandit aliquet. Etiam
-                        dictum dapibus ultricies. Sed vel aliquet libero. Nunc a
-                        augue fermentum, pharetra ligula sed, aliquam lacus.
-                      </p>
                     </div>
 
                     <div className="mt-6.5">
