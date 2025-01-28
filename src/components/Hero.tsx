6a88dab0448822c9aee1f005a19c2fea5309f787
saksham-guptaa <creator.sakshamgupta@gmail.com>
2025-01-28 10:15:28 +0530

    video change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,15 +6,15 @@ const Hero = () => {
   return (
     <div className="p-4 sm:p-6 md:p-10 lg:px-16 lg:py-12">
       <motion.div
-        className="flex flex-col lg:flex-row items-center gap-6"
+        className="flex flex-col items-center gap-6 lg:flex-row"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
       >
         {/* Main Heading */}
-        <div className="text-center relative lg:text-left flex-1">
+        <div className="relative flex-1 text-center lg:text-left">
           <motion.h1
-            className="text-3xl sm:text-4xl md:text-6xl lg:text-[80px] xl:text-[110px] py-6 font-extrabold uppercase leading-tight"
+            className="py-6 text-3xl font-extrabold uppercase leading-tight sm:text-4xl md:text-6xl lg:text-[80px] xl:text-[110px]"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.2 }}
@@ -22,7 +22,7 @@ const Hero = () => {
             NEBULA ACCELERATOR
           </motion.h1>
           <motion.span
-            className="text-xs sm:text-sm md:text-xl lg:text-3xl font-extrabold absolute right-4 bottom-2 lg:bottom-0"
+            className="absolute bottom-2 right-4 text-xs font-extrabold sm:text-sm md:text-xl lg:bottom-0 lg:text-3xl"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.5 }}
@@ -33,24 +33,23 @@ const Hero = () => {
       </motion.div>
 
       {/* Video Section */}
-      <div className="mt-8 lg:mt-12 flex flex-col lg:flex-row items-center justify-between gap-6">
-  <div className="w-full">
-    <video
-      autoPlay
-      loop
-      muted
-      playsInline
-      className="w-full h-[300px] md:h-[350px] lg:h-[600px] object-cover rounded-lg shadow-lg"
-    >
-      <source
-        src="https://res.cloudinary.com/dxwea32ih/video/upload/v1731327818/qb301sp1orhuhu1fzoyl.mp4"
-        type="video/mp4"
-      />
-      Your browser does not support the video tag.
-    </video>
-  </div>
-</div>
-
+      <div className="mt-8 flex flex-col items-center justify-between gap-6 lg:mt-12 lg:flex-row">
+        <div className="w-full">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="h-[300px] w-full rounded-lg object-cover shadow-lg md:h-[350px] lg:h-[600px]"
+          >
+            <source
+              src="https://res.cloudinary.com/dski9pira/video/upload/v1738039462/video5_jynsx9.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
     </div>
   );
 };
